0e547d4e381ca6a0de38bff027b56a2e6284a35e
ERIK MANUEL GUEVARA LADINO <36718796+Er1K6U@users.noreply.github.com>
2021-10-29 16:33:21 -0500

    Cambio de pligin en condif de WP

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const Dotend = require('dotenv-webpack');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
