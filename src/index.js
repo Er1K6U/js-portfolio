e5f6e0b56e7a75181549e90ddd9ba964bf5f098f
ERIK MANUEL GUEVARA LADINO <36718796+Er1K6U@users.noreply.github.com>
2022-01-26 10:57:55 -0500

    Paso de proyecto a React1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import Template from '@templates/Template.js';
-import '@styles/main.css';
-import '@styles/vars.styl';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+//import Template from '@templates/Template.js';
+//import '@styles/main.css';
+//import '@styles/vars.styl';
+
+ReactDOM.render(<App />)
 
 
-(async function App() {
+/* (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
-})();
+})(); */
